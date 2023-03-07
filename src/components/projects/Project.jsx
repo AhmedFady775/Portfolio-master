0c6f39f16b81ecf43f6784e2284452c600426fa0
AhmedFady775 <fadiahmed298@gmail.com>
2023-03-07 17:15:34 +0200

    Poject section optimization

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -9,38 +9,40 @@ function Project(props) {
 
   return (
     <div data-aos="fade-up" data-aos-duration="1000">
-      <div className="project__cont">
-        <div className="project__desc">
-          <div>
-            <p className="font-bold text-[#9146ff]">Featured Project</p>
-            <p className="text-2xl font-bold">{props.name}</p>
-          </div>
-          <p className="h-10">{props.desc}</p>
-          <div className="flex flex-row font-mono flex-wrap">
-            {props.made.map((item) => (
-              <div className="mr-4">{item}</div>
-            ))}
-          </div>
-          <div>
-            <span className="flex flex-row space-x-3">
-              {props.github ? (
-                <a href={props.github} target="_blank">
-                  <TbBrandGithub size={25} />
+      <a href={props.live} target="_blank">
+        <div className="project__cont">
+          <div className="project__desc">
+            <div>
+              <p className="font-bold text-[#9146ff]">Featured Project</p>
+              <p className="text-2xl font-bold">{props.name}</p>
+            </div>
+            <p className="h-10">{props.desc}</p>
+            <div className="flex flex-row font-mono flex-wrap">
+              {props.made.map((item) => (
+                <div className="mr-4">{item}</div>
+              ))}
+            </div>
+            <div>
+              <span className="flex flex-row space-x-3">
+                {props.github ? (
+                  <a href={props.github} target="_blank">
+                    <TbBrandGithub size={25} />
+                  </a>
+                ) : null}
+                <a href={props.live} target="_blank">
+                  <IoMdOpen size={25} />
                 </a>
-              ) : null}
-              <a href={props.live} target="_blank">
-                <IoMdOpen size={25} />
-              </a>
-            </span>
+              </span>
+            </div>
+          </div>
+          <div className="img__cont">
+            <img
+              className="w-full h-full object-cover rounded-md"
+              src={props.src}
+            />
           </div>
         </div>
-        <div className="img__cont">
-          <img
-            className="w-full h-full object-cover rounded-md"
-            src={props.src}
-          />
-        </div>
-      </div>
+      </a>
     </div>
   );
 }
