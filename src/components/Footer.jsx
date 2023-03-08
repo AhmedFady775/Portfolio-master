12f1dfb4f017ee154c8e71900ebe260911bdecf6
AhmedFady775 <fadiahmed298@gmail.com>
2023-03-08 15:23:39 +0200

    Footer optimization

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,10 +8,34 @@ function Footer() {
   return (
     <div className="flex flex-col py-8 items-center ">
       <div className="flex flex-row space-x-8 mb-4">
-        <FiGithub size={20} />
-        <FiLinkedin size={20} />
-        <FiFacebook size={20} />
-        <FiTwitter size={20} />
+        <a
+          className="icon__hover"
+          href="https://github.com/AhmedFady775"
+          target="_blank"
+        >
+          <FiGithub size={20} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/ahmed-fady-4a7b17248/"
+          target="_blank"
+          className="icon__hover"
+        >
+          <FiLinkedin size={20} />
+        </a>
+        <a
+          href="https://www.facebook.com/ahmed.fadi.9"
+          target="_blank"
+          className="icon__hover"
+        >
+          <FiFacebook size={20} />
+        </a>
+        {/* <a
+          href="https://github.com/AhmedFady775"
+          target="_blank"
+          className="icon__hover"
+        >
+          <FiTwitter size={20} />
+        </a> */}
       </div>
       <p className="text-violet-400">Designed & Built by Ahmed Fady</p>
     </div>
