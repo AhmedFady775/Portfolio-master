97b5a1694e370f36c4d379762117be3608a92f1a
AhmedFady775 <fadiahmed298@gmail.com>
2023-03-09 17:12:59 +0200

    Removing unwanted stuff

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { HashLink as Link } from "react-router-hash-link";
-
-// You can also use <link> for styles
 import { BsArrowDownShort } from "react-icons/bs";
 
 function Intro() {
